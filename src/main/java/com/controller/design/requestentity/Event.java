023617e3ee6c89e9db86d6490b8e9b9bc60ae1ae
Rajesh <rajesh.podder@hotmail.com>
2020-08-07 12:54:16 +0530

    Added Lombok dependency

--- src/main/java/com/controller/design/requestentity/Event.java
+++ src/main/java/com/controller/design/requestentity/Event.java
@@ -1,8 +1,16 @@
 package com.controller.design.requestentity;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.io.Serializable;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class Event implements Serializable {
 
     private String person_id;
@@ -12,63 +20,5 @@ public class Event implements Serializable {
     private String person_mobile_number;
     private String person_mobile_model;
 
-    public Event() {
-    }
 
-    public Event(String person_id, String person_fname, String person_lname, String person_email, String person_mobile_number, String person_mobile_model) {
-        this.person_id = person_id;
-        this.person_fname = person_fname;
-        this.person_lname = person_lname;
-        this.person_email = person_email;
-        this.person_mobile_number = person_mobile_number;
-        this.person_mobile_model = person_mobile_model;
-    }
-
-    public String getPerson_id() {
-        return person_id;
-    }
-
-    public void setPerson_id(String person_id) {
-        this.person_id = person_id;
-    }
-
-    public String getPerson_fname() {
-        return person_fname;
-    }
-
-    public void setPerson_fname(String person_fname) {
-        this.person_fname = person_fname;
-    }
-
-    public String getPerson_lname() {
-        return person_lname;
-    }
-
-    public void setPerson_lname(String person_lname) {
-        this.person_lname = person_lname;
-    }
-
-    public String getPerson_email() {
-        return person_email;
-    }
-
-    public void setPerson_email(String person_email) {
-        this.person_email = person_email;
-    }
-
-    public String getPerson_mobile_number() {
-        return person_mobile_number;
-    }
-
-    public void setPerson_mobile_number(String person_mobile_number) {
-        this.person_mobile_number = person_mobile_number;
-    }
-
-    public String getPerson_mobile_model() {
-        return person_mobile_model;
-    }
-
-    public void setPerson_mobile_model(String person_mobile_model) {
-        this.person_mobile_model = person_mobile_model;
-    }
 }
--- src/main/java/com/controller/design/responseentity/Response.java
+++ src/main/java/com/controller/design/responseentity/Response.java
@@ -1,32 +1,17 @@
 package com.controller.design.responseentity;
 
 
+import lombok.*;
+
 import java.io.Serializable;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class Response<T>  implements Serializable {
     private String status;
     private T messageBody;
-
-    public Response(){}
-
-    public Response(String status, T messageBody){
-        this.status=status;
-        this.messageBody=messageBody;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public T getMessageBody() {
-        return messageBody;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public void setMessageBody(T messageBody) {
-        this.messageBody = messageBody;
-    }
 }
